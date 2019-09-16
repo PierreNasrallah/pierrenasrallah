dd06f0c2c477e19c01a8920067ee8892d4342977
Pierre Nasrallah <pierrenofc@gmail.com>
2019-09-16 08:34:58 -0400

    deploying build 1

---
+++ precache-manifest.d4ca552fc8c777e952f5ba1ab3834429.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f8786c1c171a46ace862b73215990a",
+    "url": "/pierrenasrallah/index.html"
+  },
+  {
+    "revision": "531cc900e362e1e3d6ff",
+    "url": "/pierrenasrallah/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4986d25f658890104425",
+    "url": "/pierrenasrallah/static/css/main.e990435e.chunk.css"
+  },
+  {
+    "revision": "531cc900e362e1e3d6ff",
+    "url": "/pierrenasrallah/static/js/2.31dfd991.chunk.js"
+  },
+  {
+    "revision": "4986d25f658890104425",
+    "url": "/pierrenasrallah/static/js/main.582a3556.chunk.js"
+  },
+  {
+    "revision": "962959d786c49f3372cb",
+    "url": "/pierrenasrallah/static/js/runtime~main.77dfbbb4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pierrenasrallah/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "93db1ff50e871ae8031851cb958342f0",
+    "url": "/pierrenasrallah/static/media/bgGeneratorImage.93db1ff5.png"
+  },
+  {
+    "revision": "962dad193a12c290664876fb62c17c10",
+    "url": "/pierrenasrallah/static/media/faceRecognitionImage.962dad19.png"
+  },
+  {
+    "revision": "d5492d476b8a1e2fee5b0ee143182c52",
+    "url": "/pierrenasrallah/static/media/pierre.d5492d47.jpg"
+  },
+  {
+    "revision": "dc694ed83c83525ddd2d5082a1e25d8b",
+    "url": "/pierrenasrallah/static/media/roboFriendsImage.dc694ed8.png"
+  }
+]);
